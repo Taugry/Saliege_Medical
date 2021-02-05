0f156227dcd93d117fe8f9772cbbe1cef3868af1
Téo Augry <teo.augry@limayrac.fr>
2021-02-05 09:36:01 +0100

    Ajout des dossiers de config, css, js et des packages .NET

---
+++ js/main.js
@@ -0,0 +1,9 @@
+﻿function myFunction(nb) {
+    var checkBox = document.getElementById("myCheck"+nb);
+    var text = document.getElementById("res" + nb);
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
